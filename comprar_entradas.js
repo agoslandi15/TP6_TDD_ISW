78f1b0bc5656f6172d599f8d815fea60e07a591f
franfigueroa-17 <franciscofigueroa303@gmail.com>
2025-10-14 19:53:39 -0300

    agos gil

---
+++ comprar_entradas.js
@@ -0,0 +1,14 @@
+// comprar_entradas.js
+function validar_cant_entradas(cantidad) {
+	if (typeof cantidad !== 'number' || Number.isNaN(cantidad)) {
+		throw new TypeError('cantidad debe ser un número');
+	}
+
+	if (cantidad > 10) {
+		return "No puede superar las 10 entradas";
+	} else {
+		return "Se ingresó correctamente la cantidad de entradas";
+	}
+}
+
+module.exports = validar_cant_entradas;
---
+++ test_comprar_entradas.test.js
@@ -0,0 +1,22 @@
+// test_comprar_entradas.test.js
+const validar = require('./comprar_entradas');
+
+test('acepta 0', () => {
+    var cantidadEntradas = 0;
+  expect(validar(cantidadEntradas)).toBe('Se ingresó correctamente la cantidad de entradas');
+});
+
+test('acepta 10', () => {
+    // Precondiciones
+  var cantidadEntradas = 10;
+  // Llamada a la función
+  var resultado = validar(cantidadEntradas);
+  // Resultados
+  expect(resultado).toBe('Se ingresó correctamente la cantidad de entradas');
+});
+
+test('rechaza 11', () => {
+  var cantidadEntradas = 11;
+  var resultado = validar(cantidadEntradas);
+  expect(resultado).toBe('No puede superar las 10 entradas');
+});
