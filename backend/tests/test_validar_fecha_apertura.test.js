7748fb4c263958f7fb7a1386e0c8cb7005c0c5af
AntonellaPedraza24 <antopedraza028@gmail.com>
2025-10-14 21:14:26 -0300

    test validar_fecha_apertura

--- backend/tests/test_validar_fecha_apertura.test.js
+++ backend/tests/test_validar_fecha_apertura.test.js
@@ -1,3 +1,28 @@
-test('dummy test', () => {
-  expect(true).toBe(true);
+const { validar_fecha_apertura } = require('../utils/validar_fecha_apertura');
+
+test('fecha invalida dias lunes', () => {
+  const fecha = new Date(2024, 9, 7);
+  expect(validar_fecha_apertura(fecha)).toBe('Parque cerrado los días lunes');
+});
+
+
+test('fecha invalida 25 de diciembre', () => {
+  const fecha = new Date();
+  fecha.setDate(25);
+  fecha.setMonth(11);
+  expect(validar_fecha_apertura(fecha)).toBe('Parque cerrado el 25 de diciembre');
+});
+
+test('fecha invalida 1 de enero', () => {
+  const fecha = new Date();
+  fecha.setDate(1);
+  fecha.setMonth(0);
+  expect(validar_fecha_apertura(fecha)).toBe('Parque cerrado el 1 de enero');
+});
+
+test('fecha valida', () => {
+  const hoy = new Date();
+  const fecha = new Date();
+  fecha.setDate(hoy.getDate() + 15);
+  expect(validar_fecha_apertura(fecha)).toBe('Se ingresó correctamente la fecha de compra');
 });
--- backend/utils/validar_fecha_apertura.js
+++ backend/utils/validar_fecha_apertura.js
@@ -1,5 +1,23 @@
-function validar_fecha_apertura(fecha) {
+function validar_fecha_apertura(fecha_apertura) {    
+	const hoy = new Date();
+	hoy.setHours(0, 0, 0, 0); 
+
+	const fecha_limite = new Date();
+	fecha_limite.setMonth(fecha_limite.getMonth() + 1);
+
+	if (fecha_apertura.getDay() === 1) {
+		return "Parque cerrado los días lunes";
+	}
+
+	if (fecha_apertura.getDate() === 25 && fecha_apertura.getMonth() === 11) {
+		return "Parque cerrado el 25 de diciembre";
+	}
+	if (fecha_apertura.getDate() === 1 && fecha_apertura.getMonth() === 0) {
+		return "Parque cerrado el 1 de enero";
+	}
+	
+    return 'Se ingresó correctamente la fecha de compra';
+
 }
 
-
-module.exports = validar_fecha_apertura;
+module.exports = { validar_fecha_apertura };
